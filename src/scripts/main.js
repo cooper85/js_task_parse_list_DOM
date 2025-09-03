16bc8d45ae3dcc9578b14c85cc7fcfad91548d05
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-09-04 00:15:13 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
-import { parseSalary }
-  from './helpers/parseSalary.js';
+import { parseSalary } from './helpers/parseSalary.js';
 
 /**
  * Sort list items by salary in-place
@@ -8,14 +7,17 @@ import { parseSalary }
  */
 function sortList(elTarget) {
   const listItems = Array.from(elTarget.children);
+
   listItems.forEach((el) => {
     el.dataset.salary = el.dataset.salary || '0';
     el.dataset.position = el.dataset.position || '';
     el.dataset.age = el.dataset.age || '0';
   });
-  listItems.sort((a,b)=>parseSalary(b.dataset.salary)
-    -parseSalary(a.dataset.salary));
-  listItems.forEach((el)=>elTarget.appendChild(el));
+
+  listItems.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+  listItems.forEach((el) => elTarget.appendChild(el));
 }
 
 /**
@@ -24,18 +26,18 @@ function sortList(elTarget) {
  * @return {Array<Object>}
  */
 function getEmployees(elTarget) {
-  return Array.from(elTarget.querySelectorAll(':scope > li'))
-    .map((el)=>({
-      name: el.textContent.trim(),
-      position: el.dataset.position||undefined,
-      salary: parseSalary(el.dataset.salary),
-      age: !isNaN(el.dataset.age)?Number(el.dataset.age):0,
-    }));
+  return Array.from(elTarget.querySelectorAll(':scope > li')).map((el) => ({
+    name: el.textContent.trim(),
+    position: el.dataset.position || undefined,
+    salary: parseSalary(el.dataset.salary),
+    age: !isNaN(el.dataset.age) ? Number(el.dataset.age) : 0,
+  }));
 }
 
 // Select container
-const targetElement=document.querySelector('ul');
+const targetElement = document.querySelector('ul');
+
 sortList(targetElement);
 
 // eslint-disable-next-line no-unused-vars
-const employees=getEmployees(targetElement);
+const employees = getEmployees(targetElement);
